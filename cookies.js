f777f36b9166c77ac8d10950a06fa39e1a2b947c
7trail <72806227+7trail@users.noreply.github.com>
2024-01-25 19:28:39 -0600

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,21 @@
+function readCookie(name) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(name + '=') === 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+function writeCookie(name, value) {
+    var expires = '';
+	var daysToExpire = 90;
+    if (daysToExpire) {
+        var date = new Date();
+        date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+}
